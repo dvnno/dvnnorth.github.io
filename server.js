08e1ca0aa04b5cf88b69f2652bb42ac436a7219a
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-09-09 00:18:52 -0500

    Corrected config file for Sequelize

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ app.set('view engine', 'handlebars');
 
 require(path.join(__dirname, "/controllers/routes"))(app);
 
-console.log(process.env.JAWSDB_URL);
-
 if (process.env.JAWSDB_URL) {
     syncDb(false);
 }
